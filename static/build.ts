8aa2bb60bbc8873d2054cd0ce8b9ee5533318e2b
arshad <arshadpyaseen@gmail.com>
2025-10-09 17:26:10 +0530

    chore: update

--- static/build.ts
+++ static/build.ts
@@ -13,6 +13,6 @@ await Bun.build({
 	entrypoints: ["chat/index.tsx"],
 	outdir: "dist/chat",
 	format: "esm",
-	minify: process.env.PRODUCTION === "true",
+	minify: true,
 	plugins: [tailwindcss, copyHtmlPlugin],
 });
