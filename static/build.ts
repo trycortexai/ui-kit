65d006df34a01bdc387ac986fd0f706614f2a2c7
arshad <arshadpyaseen@gmail.com>
2025-10-10 18:32:55 +0530

    chore: update

--- static/build.ts
+++ static/build.ts
@@ -1,19 +1,19 @@
 import tailwindcss from "@bunup/plugin-tailwindcss";
 
-await Bun.$`rm -rf dist/**`;
+await Bun.$`rm -rf dist`;
 await Bun.$`mkdir -p dist/chat`;
 
 const copyHtmlPlugin: Bun.BunPlugin = {
 	name: "copy-html",
 	setup(build) {
 		build.onEnd(async () => {
-			await Bun.$`cp static/index.html ${build.config.outdir}/index.html`;
+			await Bun.$`cp index.html ${build.config.outdir}/index.html`;
 		});
 	},
 };
 
 await Bun.build({
-	entrypoints: ["static/apps/chat/index.tsx"],
+	entrypoints: ["apps/chat/index.tsx"],
 	outdir: "dist/chat",
 	format: "esm",
 	minify: true,
