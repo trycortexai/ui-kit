6c0e9ed55e53da722338dc26c5d90294c88663bf
arshad <arshadpyaseen@gmail.com>
2025-10-16 23:27:29 +0530

    chore: update

--- packages/ui-kit-shared/src/index.ts
+++ packages/ui-kit-shared/src/index.ts
@@ -1,9 +1,9 @@
-export const UI_KIT_DEV_SERVER_PORT = 3001;
+export const UI_KIT_DEV_SERVER_PORT = 8080;
 
 export const UI_KIT_API_URL: string =
 	process.env.NODE_ENV === "development"
 		? `http://localhost:${UI_KIT_DEV_SERVER_PORT}`
-		: "https://onrender.com";
+		: "https://cortex-aiui-kit-server-production.up.railway.app/";
 
 export const UI_KIT_SCRIPT_URL =
 	"https://unpkg.com/@cortex-ai/ui-kit@latest/dist/index.js";
