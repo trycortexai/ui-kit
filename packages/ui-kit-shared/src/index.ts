4948ada05815501ce08d44ae8523851fbb23bfe2
arshad <arshadpyaseen@gmail.com>
2025-10-18 03:13:46 +0530

    chore: use only production server url

--- packages/ui-kit-shared/src/index.ts
+++ packages/ui-kit-shared/src/index.ts
@@ -1,9 +1,7 @@
 export const UI_KIT_DEV_SERVER_PORT = 8080;
 
 export const UI_KIT_API_URL: string =
-	process.env.NODE_ENV === "development"
-		? `http://localhost:${UI_KIT_DEV_SERVER_PORT}`
-		: "https://cortex-aiui-kit-server-production.up.railway.app/";
+	"https://cortex-aiui-kit-server-production.up.railway.app/";
 
 export type CortexUIKitOptions = {
 	/**
